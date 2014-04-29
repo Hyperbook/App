d907b4e984fbc226db4e4e831fc17781ecb327c4
WH <weldon23.henson23@gmail.com>
2014-04-29 11:30:49 -0400

    Update login_github.js

--- js/login_github.js
+++ js/login_github.js
@@ -1,8 +1,8 @@
-function ArcherCraftGithubLogin(){
+$(document).ready(function(){
 var github = new Github({
   username: $( "input:text[name=username]" ).val();,
   password: $( "input:text[name=username]" ).val();,
-  auth: "basic"
+  auth: "basic",
 });
-}
+});
  
